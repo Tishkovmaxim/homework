6a6fcb14291cc5db9ed79867b0856ed141c53572
Tishkovmaxim <90570691+Tishkovmaxim@users.noreply.github.com>
2021-12-13 12:50:48 +0300

    Update hw2.cpp

--- homework2/hw2.cpp
+++ homework2/hw2.cpp
@@ -12,7 +12,6 @@ int main(int argc, char** argv) {
 
         // есть один агрумент
         // в argv[1] содержится строка с первым агрументом (имя файла)
-        std::cout << "1st argument: " << argv[1] << std::endl;
         filename = argv[1];
     } else {
         // аргументов нет или их больше чем мы ожидаем
